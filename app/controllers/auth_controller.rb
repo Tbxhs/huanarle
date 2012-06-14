32e1ca86b19bcc503135006d636eec6e0e7aef76
Huang Jin <huangjin@localhost.(none)>
2012-06-14 10:07:10 +0800

    update a tweet to weibo

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,7 +6,7 @@ class AuthController < ApplicationController
       auth = request.env["omniauth.auth"]
       user = User.find_or_create_from_auth(auth)
       self.current_user = user
-      user.sync_to_weibo 
+      # user.sync_to_weibo 
     rescue => e
       # raise e
       flash[:error] = '授权过程中出错，请重试'
